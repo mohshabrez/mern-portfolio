7b6d79c1fe22acd158e9ed13536f39bd0e6a1549
Shabrez G <31725739+mohshabrez@users.noreply.github.com>
2024-01-16 17:13:26 +0530

    Update Main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { MernProjects } from './Projects/MernProjects';
+import  {Main}  from './Pages/Main';
+import { FrontProjects } from './Projects/FrontProjects';
+import { BlogSection } from './Projects/BlogSection';
+import { JavascriptSection } from './Projects/JavascriptSection';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Main/>}/>
+        <Route path='/mernProjects' element={<MernProjects/>}/>
+        <Route path='/frontProjects' element={<FrontProjects/>}/>
+        <Route path='/blogSection' element={<BlogSection/>}/>
+        <Route path='/javascript' element={<JavascriptSection/>}/>
+      </Routes>
     </div>
   );
 }
